78794dd5d37b82e4efbfe2a36714149b2a44254b
switzerb <brenna.switzer@gmail.com>
2019-02-05 20:24:30 -0800

    Removing unintended tracking from git

---
+++ react-nfp/build/precache-manifest.f602e0030fe6a6156cdaf458b29197d5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "918104570062c896be49",
+    "url": "/static/js/main.91810457.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "918104570062c896be49",
+    "url": "/static/css/main.31d4308e.css"
+  },
+  {
+    "revision": "25cfb5c219de56372bd01d20781ec163",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2f717775d17bf3a581acf2903353545f",
+    "url": "/../../rankings/static/build/manifest.json"
+  },
+  {
+    "revision": "c92b85a5b907c70211f4ec25e29a8c4a",
+    "url": "/../../rankings/static/build/favicon.ico"
+  }
+];
